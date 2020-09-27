a76882f044b5ab37d55e86a8f8dccc4f452775dc
Christian Gleissner <chrisgleissner@gmail.com>
2020-09-27 17:09:03 +0200

    Wildcard search

--- elastic-spring/src/main/java/com/github/chrisgleissner/elastic/spring/book/CustomBookRepo.java
+++ elastic-spring/src/main/java/com/github/chrisgleissner/elastic/spring/book/CustomBookRepo.java
@@ -2,7 +2,7 @@ package com.github.chrisgleissner.elastic.spring.book;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
@@ -13,17 +13,18 @@ import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component @RequiredArgsConstructor @Slf4j
 public class CustomBookRepo {
     private final ElasticsearchRestTemplate restTemplate;
 
-    public List<Book> findByNameAndAuthorWithWildcards(String name, String author) {
-        QueryBuilder qb = QueryBuilders.boolQuery()
-                .filter(QueryBuilders.wildcardQuery(Book.NAME, name))
-                .filter(QueryBuilders.wildcardQuery(Book.AUTHOR, author));
-        Query query = new NativeSearchQuery(qb).setPageable(Pageable.unpaged());
+    public List<Book> findByOptionalWildcardArgs(String name, String author) {
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        Optional.ofNullable(name).ifPresent(n -> boolQueryBuilder.filter(QueryBuilders.wildcardQuery(Book.NAME, n)));
+        Optional.ofNullable(author).ifPresent(a -> boolQueryBuilder.filter(QueryBuilders.wildcardQuery(Book.AUTHOR, a)));
+        Query query = new NativeSearchQuery(boolQueryBuilder).setPageable(Pageable.unpaged());
         SearchHits<Book> hits = restTemplate.search(query, Book.class);
         List<Book> books = hits.get().map(SearchHit::getContent).collect(Collectors.toList());
         log.info("findByNameAndAuthorWithWildcards(name={}, author={}) found {} hit(s): hits={}, books={}",
--- elastic-spring/src/test/java/com/github/chrisgleissner/elastic/spring/book/BookIT.java
+++ elastic-spring/src/test/java/com/github/chrisgleissner/elastic/spring/book/BookIT.java
@@ -7,70 +7,81 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 
 import java.util.List;
 
+import static com.github.chrisgleissner.elastic.spring.book.BookIT.TestData.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS) @Slf4j
 class BookIT extends AbstractElasticIT {
-    public static final String ISBN1 = "11";
-    public static final String ISBN2 = "12";
-    public static final String ISBN3 = "21";
-
-    private static final String NAME_COMMON_PREFIX = "book name";
-    private static final String NAME1 = NAME_COMMON_PREFIX + " 11";
-    private static final String NAME2 = NAME_COMMON_PREFIX + " 12";
-    private static final String NAME3 = NAME_COMMON_PREFIX + " 21";
-
-    private static final String AUTHOR_COMMON_PREFIX = "joe";
-    private static final String AUTHOR1 = AUTHOR_COMMON_PREFIX + " doe";
-    private static final String AUTHOR2 = AUTHOR_COMMON_PREFIX + " don doe";
-
-    private static final int PUBLICATION_YEAR_1 = 2001;
-    private static final int PUBLICATION_YEAR_2 = 2002;
-    private static final int PUBLICATION_YEAR_3 = 2003;
-
-    private static final Book BOOK1 = new Book(ISBN1, NAME1, AUTHOR1, PUBLICATION_YEAR_1);
-    private static final Book BOOK2 = new Book(ISBN2, NAME2, AUTHOR1, PUBLICATION_YEAR_2);
-    private static final Book BOOK3 = new Book(ISBN3, NAME3, AUTHOR2, PUBLICATION_YEAR_3);
-
-    @Autowired ElasticsearchRestTemplate restTemplate;
     @Autowired BookRepo bookRepo;
     @Autowired CustomBookRepo customBookRepo;
+
+    @Test
+    void findByAuthor() {
+        // Exact match since author is of type keyword. If we this field were of type text, we'd get all 3 books.
+        assertThat(bookRepo.findByAuthor(AUTHOR1)).containsExactly(BOOK1, BOOK2);
+    }
+
+    @Test
+    void findByAuthorStartingWith() {
+        assertThat(bookRepo.findByAuthorStartingWith(AUTHOR_COMMON_PREFIX)).containsExactlyElementsOf(BOOKS);
+    }
+
+    @Test
+    void findByOptionalWildcardArgsUsingNameAndAuthor() {
+        assertThat(customBookRepo.findByOptionalWildcardArgs(NAME_COMMON_PREFIX + "*", AUTHOR1)).containsExactly(BOOK1, BOOK2);
+    }
+
+    @Test
+    void findByOptionalWildcardArgsUsingAuthor() {
+        assertThat(customBookRepo.findByOptionalWildcardArgs(null, AUTHOR_COMMON_PREFIX + "*")).containsExactlyElementsOf(BOOKS);
+    }
+
+    @Test
+    void findByOptionalWildcardArgsUsingAuthorWithLeadingWildcard() {
+        assertThat(customBookRepo.findByOptionalWildcardArgs(null, "*" + AUTHOR2_UNIQUE_STRING + "*")).containsExactly(BOOK3);
+    }
+
+    interface TestData {
+        String ISBN1 = "11";
+        String ISBN2 = "12";
+        String ISBN3 = "21";
+
+        String NAME_COMMON_PREFIX = "book name";
+        String NAME1 = NAME_COMMON_PREFIX + " 11";
+        String NAME2 = NAME_COMMON_PREFIX + " 12";
+        String NAME3 = NAME_COMMON_PREFIX + " 21";
+
+        String AUTHOR_COMMON_PREFIX = "joe";
+        String AUTHOR1 = AUTHOR_COMMON_PREFIX + " doe";
+        String AUTHOR2_UNIQUE_STRING = "don";
+        String AUTHOR2 = AUTHOR_COMMON_PREFIX + " " + AUTHOR2_UNIQUE_STRING + " doe";
+
+        int PUBLICATION_YEAR_1 = 2001;
+        int PUBLICATION_YEAR_2 = 2002;
+        int PUBLICATION_YEAR_3 = 2003;
+
+        // Written by joe doe:
+        Book BOOK1 = new Book(ISBN1, NAME1, AUTHOR1, PUBLICATION_YEAR_1);
+        Book BOOK2 = new Book(ISBN2, NAME2, AUTHOR1, PUBLICATION_YEAR_2);
+
+        // Written by joe don doe:
+        Book BOOK3 = new Book(ISBN3, NAME3, AUTHOR2, PUBLICATION_YEAR_3);
+
+        List<Book> BOOKS = List.of(BOOK1, BOOK2, BOOK3);
+    }
 
     @BeforeAll
     void setup() {
         getFixture().updateIndexConfig(Book.class, Book.INDEX_NAME);
-
-        bookRepo.saveAll(List.of(
-                // Written by joe doe:
-                BOOK1, // book name 11, 2001
-                BOOK2, // book name 12, 2002
-                // Written by joe don doe:
-                BOOK3 // book name21, 2003
-        ));
+        bookRepo.saveAll(BOOKS);
         getFixture().logIndexMapping(Book.INDEX_NAME);
     }
 
     @AfterAll
     void tearDown() {
         bookRepo.deleteAll();
-    }
-
-    @Test
-    void findByAuthor() {
-        assertThat(bookRepo.findByAuthor(AUTHOR1)).containsExactly(BOOK1, BOOK2);
-    }
-
-    @Test
-    void findByAuthorStartingWithPrefix() {
-        assertThat(bookRepo.findByAuthorStartingWith(AUTHOR_COMMON_PREFIX)).containsExactly(BOOK1, BOOK2, BOOK3);
-    }
-
-    @Test
-    void findByNameAndAuthorWithWildcards() {
-        assertThat(customBookRepo.findByNameAndAuthorWithWildcards(NAME_COMMON_PREFIX + "*", AUTHOR1)).containsExactly(BOOK1, BOOK2);
     }
 }
